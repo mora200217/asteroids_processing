93eb910d5db9539ef0cc590d2a7edc349b72683c
GokuSJJ2000 <ptorresro@unal.edu.co>
2022-12-01 21:57:59 -0500

    Update: Hitbox __init__

---
+++ source/python/hitbox.py
@@ -0,0 +1,9 @@
+class Hitbox:
+    def __init__(self, x=81, y=81,  ancho=63, alto=80, visible=False, color = 0, grosor = 3):
+        self.visible = visible
+        self.x = x
+        self.y = y
+        self.ancho = ancho
+        self.alto = alto
+        self.color = color
+        self.grosor = grosor
