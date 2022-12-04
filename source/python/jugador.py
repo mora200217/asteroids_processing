ef35e90e05250b7458690919952dea3fafcd4fad
GokuSJJ2000 <ptorresro@unal.edu.co>
2022-12-04 14:39:24 -0500

    Update: Jugador - Agregado deteccion de input

--- source/python/jugador.py
+++ source/python/jugador.py
@@ -29,3 +29,38 @@ class Jugador:
 
     def girarDerecha(self):
         self.setAngulo(self.getAngulo() - (10 * self.velocidadGiro))
+
+
+    def update(self):
+        if not keyPressed:
+            return
+            
+        if key == "a":
+            self.girarDerecha()
+        elif key == "d":
+            self.girarIzquierda()
+
+        if key == "w":
+            catetoOpuesto = sin(radians(self.angulo))
+            catetoAdyacente = sqrt(1 - pow(catetoOpuesto, 2))
+
+            if self.angulo > 90 and self.angulo < 270:
+                catetoAdyacente = catetoAdyacente * -1
+
+            self.x = self.x + (catetoAdyacente * self.velocidad)
+            self.y = self.y + (catetoOpuesto * self.velocidad)
+
+            if self.x > width + 20:
+                self.x = -20
+            if self.x < -20:
+                self.x = width + 20
+            if self.y > height + 20:
+                self.y = -20
+            if self.y < -20:
+                self.y = height + 20
+
+    def display(self):
+        stroke(204, 102, 0)
+        translate(self.x, self.y)
+        rotate(radians(self.angulo + 90))
+        triangle(- 10, 20, 0, - 20,   10,  20)
