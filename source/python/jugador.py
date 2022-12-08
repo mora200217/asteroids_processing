f858b135b84a688bdebdceb66c7f9ff6b0ee8d8f
GokuSJJ2000 <ptorresro@unal.edu.co>
2022-12-08 00:13:40 -0500

    Merge from f/python/jugador-movimiento

--- source/python/jugador.py
+++ source/python/jugador.py
@@ -1,7 +1,44 @@
 
 
 class Jugador:
-    def __init__(self, x, y, hitbox, lista_keys, velocidadMaxima=2, aceleracion=0.1, friccion=0.1, velocidadGiro=1):
+    def __init__(self, x, y, hitbox, lista_keys, velocidadMaxima=2, aceleracion=0.1, friccion=0.1, velocidadMaximaGiro=10, aceleracionGiro=0.1, friccionGiro=0.1):
+        """
+        Parameters
+        ----------
+        x : int o float
+            Coordenada x inicial del Jugador.
+
+        y : int o float
+            Coordenada y inicial del Jugador.
+
+        hitbox : Hitbox
+            Hitbox para las colisiones.
+
+        lista_keys : list of keys
+            Lista que contiene las keys presionadas durante el programa.
+        
+        --------------------------------------------
+        
+        velocidadMaxima : int o float, default 2
+            Velocidad maxima que alcanza el jugador.
+        
+        aceleracion : int o float, default 0.1
+            Aceleracion que posee el jugador.
+        
+        friccion : int o float, default 0.1
+            Friccion que se le aplica al jugador para frenarlo.
+        
+        --------------------------------------------
+        
+        velocidadMaximaGiro : int o float, default 0.1
+            Velocidad maxima alcanzada al rotar sobre el propio eje, en grados.
+        
+        aceleracionGiro : int o float, default 0.1
+            Aceleracion que se aplica al rotar sobre si mismo.
+        
+        friccionGiro : int o float, default 0.1
+            Friccion que se le aplica al jugador para que deje de girar.      
+        """
         self.x = x
         self.y = y
 
@@ -13,12 +50,18 @@ class Jugador:
         self.dx = 0
         self.dy = 0
 
+        # Ajustando parametros velocidad giro inicial
         self.angulo = 0
-        self.velocidadGiro = velocidadGiro
-
+        self.velocidadGiro = 0
+        self.velocidadMaximaGiro = velocidadMaximaGiro
+        self.aceleracionGiro = aceleracionGiro
+        self.friccionGiro = friccionGiro
+        
+        # Acomodando hitbox
         self.hitbox = hitbox
         self.hitbox.setPosicion(x, y)
 
+        # Teclas presionadas
         self.lista_keys = lista_keys
         self.teclas = {"izquierda": "a",
                        "derecha": "d",
@@ -49,18 +92,45 @@ class Jugador:
         return self.angulo
 
     def girarIzquierda(self):
-        self.setAngulo(self.getAngulo() - (10 * self.velocidadGiro))
+        self.velocidadGiro = self.velocidadGiro - self.aceleracionGiro
+        
+        if self.velocidadGiro > self.velocidadMaximaGiro:
+            self.velocidadGiro = self.velocidadMaximaGiro
+
+        self.setAngulo(self.getAngulo() + self.velocidadGiro)
 
     def girarDerecha(self):
-        self.setAngulo(self.getAngulo() + (10 * self.velocidadGiro))
+        self.velocidadGiro = self.velocidadGiro + self.aceleracionGiro
+        
+        if self.velocidadGiro > self.velocidadMaximaGiro:
+            self.velocidadGiro = self.velocidadMaximaGiro
+
+        self.setAngulo(self.getAngulo() + self.velocidadGiro)
+
+    def frenarGiro(self):
+        if self.velocidadGiro > 0:
+            self.velocidadGiro = self.velocidadGiro - self.friccionGiro
+            
+            if self.velocidadGiro < 0:
+                self.velocidadGiro = 0
+
+        elif self.velocidadGiro < 0:
+            self.velocidadGiro = self.velocidadGiro + self.friccionGiro
+            
+            if self.velocidadGiro > 0:
+                self.velocidadGiro = 0
+
+        self.setAngulo(self.getAngulo() + self.velocidadGiro)
 
     def acelerar(self):
         self.velocidad = self.velocidad + self.aceleracion
+        
         if self.velocidad >= self.velocidadMaxima:
             self.velocidad = self.velocidadMaxima
 
     def frenar(self):
         self.velocidad = self.velocidad - self.friccion
+        
         if self.velocidad < 0:
             self.velocidad = 0
 
@@ -72,9 +142,9 @@ class Jugador:
             catetoAdyacente = catetoAdyacente * -1
 
         return catetoOpuesto, catetoAdyacente
-    
+
     def calcularPocicion(self):
-        catetoOpuesto,catetoAdyacente = self.getCatetos()
+        catetoOpuesto, catetoAdyacente = self.getCatetos()
 
         self.dx = catetoAdyacente * self.velocidad
         self.dy = catetoOpuesto * self.velocidad
@@ -82,10 +152,12 @@ class Jugador:
         y = self.y + self.dy
 
         self.setPosicion(x, y)
-        
+
     def update(self):
+        print(self.velocidadGiro, self.angulo)
         if not keyPressed:
             self.frenar()
+            self.frenarGiro()
             self.calcularPocicion()
             return
 
@@ -95,13 +167,16 @@ class Jugador:
         elif self.teclas["derecha"] in self.lista_keys:
             self.girarDerecha()
 
+        else:
+            self.frenarGiro()
+
         if self.teclas["avanzar"] in self.lista_keys:
             self.acelerar()
-            self.calcularPocicion()
 
         else:
             self.frenar()
-            self.calcularPocicion()
+
+        self.calcularPocicion()
 
     def display(self):
         self.hitbox.display()
